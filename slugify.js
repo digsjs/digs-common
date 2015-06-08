f26e344d45e45e422b7631748bf0d8553b70e543
Christopher Hiller <boneskull@boneskull.com>
2015-06-08 00:03:46 -0700

    zap lodash-mixins; move slugify code; zap index.js

---
+++ slugify.js
@@ -0,0 +1,24 @@
+'use strict';
+
+let _ = require('lodash'),
+  slug = require('slug');
+
+let slugs = new Set(),
+  debug = require('debug')('digs-common:slugify');
+
+module.exports = function slugify(str) {
+  if (!_.isString(str)) {
+    return str;
+  }
+  let slugged = slug(str),
+    slugified = slugged;
+  while (slugs.has(slugified)) {
+    slugified = _.uniqueId(slugged + '-');
+  }
+  slugified = slugified.toLowerCase();
+  slugs.add(slugified);
+  if (str !== slugified) {
+    debug(`slugified: "${str}" => "${slugified}"`);
+  }
+  return slugified;
+};
