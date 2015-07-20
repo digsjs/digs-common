005f5b035bde6a9c4a33dd6f459ea22ece306acd
Christopher Hiller <boneskull@boneskull.com>
2015-07-19 22:32:39 -0700

    remove annoying debug from slugify module

--- slugify.js
+++ slugify.js
@@ -1,24 +1,19 @@
 'use strict';
 
-let _ = require('lodash'),
-  slug = require('slug');
-
-let slugs = new Set(),
-  debug = require('debug')('digs:digs-common:slugify');
+let _ = require('lodash');
+let slug = require('slug');
+let slugs = new Set();
 
 module.exports = function slugify(str) {
   if (!_.isString(str)) {
     return str;
   }
-  let slugged = slug(str),
-    slugified = slugged;
+  let slugged = slug(str);
+  let slugified = slugged;
   while (slugs.has(slugified)) {
     slugified = _.uniqueId(slugged + '-');
   }
   slugified = slugified.toLowerCase();
   slugs.add(slugified);
-  if (str !== slugified) {
-    debug(`slugified: "${str}" => "${slugified}"`);
-  }
   return slugified;
 };
