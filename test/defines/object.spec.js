c6137f8e98525c217a41c5c2f91ec60317beb14d
Christopher Hiller <boneskull@boneskull.com>
2015-09-14 21:35:22 -0700

    no tests for abstract classes!

--- test/defines/object.spec.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-let DigsObject = require('../../lib/defines').DigsObject;
-let digsObjectSuite = require('./suites/object');
-let _ = require('../../lib').utils;
-
-describe('defines.DigsObject', _.partial(digsObjectSuite, DigsObject));
