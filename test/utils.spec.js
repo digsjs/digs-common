9f11e06563ac1145a605bd5f9557776e24b8887d
Christopher Hiller <boneskull@boneskull.com>
2015-10-21 21:25:28 -0700

    add isMultiple()

--- test/utils.spec.js
+++ test/utils.spec.js
@@ -91,4 +91,58 @@ describe('utils', () => {
       expect(_.isSingular()).to.be.false;
     });
   });
+
+  describe(`isMultiple()`, () => {
+    it(`should be a function`, () => {
+      expect(_.isMultiple).to.be.a('function');
+    });
+
+    it(`should return false on an empty array`, () => {
+      expect(_.isMultiple([])).to.be.false;
+    });
+
+    it(`should return false on an empty object`, () => {
+      expect(_.isMultiple({})).to.be.false;
+    });
+
+    it(`should return true on an array with more than one member`, () => {
+      expect(_.isMultiple([1, 2])).to.be.true;
+    });
+
+    it(`should return true on an object with more than one property`, () => {
+      expect(_.isMultiple({foo: 'bar', baz: 'quux'})).to.be.true;
+    });
+
+    it(`should return false on an array with one member`, () => {
+      expect(_.isMultiple([1])).to.be.false;
+    });
+
+    it(`should return false on an object with one member`, () => {
+      expect(_.isMultiple({foo: 'bar'})).to.be.false;
+    });
+
+    it(`should return true on a two-character string`, () => {
+      expect(_.isMultiple('pi')).to.be.true;
+    });
+
+    it(`should return false on a one-character string`, () => {
+      expect(_.isMultiple('f')).to.be.false;
+    });
+
+    it(`should return false on any number`, () => {
+      expect(_.isMultiple(0)).to.be.false;
+      expect(_.isMultiple(1)).to.be.false;
+      expect(_.isMultiple(-1)).to.be.false;
+      expect(_.isMultiple(2)).to.be.false;
+      expect(_.isMultiple(Infinity)).to.be.false;
+    });
+
+    it(`should return false on null`, () => {
+      expect(_.isMultiple(null)).to.be.false;
+    });
+
+    it(`should return false on undefined`, () => {
+      expect(_.isMultiple()).to.be.false;
+    });
+  });
 });
